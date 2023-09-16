bced4567cc897605c3dd4900ec35731cccc93430
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2023-09-16 16:17:58 -0300

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,28 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    bscscan: API_KEY,
+  },
   networks: {
-    // development: {
-    //   host: "localhost",
-    //   port: 8545,
-    //   network_id: "*", // Match any network id
-    //   gas: 5000000
-    // }
+    bsctest: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonic,
+          },
+          providerOrUrl: "https://data-seed-prebsc-1-s1.binance.org:8545",
+        }),
+      skipDryRun: true,
+      network_id: "97", // Match any network id
+      // websockets: true,
+      // gas: 5000000
+    },
   },
   compilers: {
     solc: {
@@ -13,9 +30,9 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true, // Default: false
-          runs: 200      // Default: 200
+          runs: 200, // Default: 200
         },
-      }
-    }
-  }
+      },
+    },
+  },
 };
